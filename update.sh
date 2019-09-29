737cbcb5d1a058025d3e840732cff5998c4b2dc1
Vadim Evard <v.e.evard@gmail.com>
2019-09-29 17:42:13 +0300

    Add update.sh script

---
+++ update.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -eu -o pipefail
+set -x
+
+pushd() {
+    command pushd "$@" > /dev/null
+}
+
+popd() {
+    command popd "$@" > /dev/null
+}
+
+update-to-latest-tag() {
+	pushd "$1"
+	git fetch --all
+	git checkout "$(git describe  --abbrev=0 origin/master)"
+	popd
+}
+
+get-tag() {
+	pushd "$1"
+	git describe  --abbrev=0 origin/master
+	popd
+}
+
+update-to-latest-tag openssl
+update-to-latest-tag curl
+
+openssl_tag="$(get-tag openssl)"
+curl_tag="$(get-tag curl)"
+git commit -am "curl $curl_tag openssl $openssl_tag"
+#git push
