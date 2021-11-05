f345890d806a17a608277099b04cb5d8b0fb4807
Vadim Evard <v.e.evard@gmail.com>
2021-11-05 17:57:11 +0300

    curl curl-7_79_1 openssl openssl-3.0.0

--- update.sh
+++ update.sh
@@ -26,7 +26,7 @@ get-tag() {
 git pull
 
 update-to-latest-tag curl master
-update-to-latest-tag openssl OpenSSL_1_1_1-stable
+update-to-latest-tag openssl openssl-3.0
 
 openssl_tag="$(get-tag openssl)"
 curl_tag="$(get-tag curl)"
